805cbaf7bd7f59284a000f3f9543fc360a972e65
Erick Barros Ferreira Gomes <erickgomes.developer@gmail.com>
2025-05-27 22:12:58 -0300

    refactor(reserva): atualiza implementação do ReservaRepository"

--- backend/src/repositories/ReservaRepository.ts
+++ backend/src/repositories/ReservaRepository.ts
@@ -1,6 +1,6 @@
 import type { Reserva } from '../entities/Reserva'
 
 export interface ReservaRepository {
-	create(reserva: Reserva): Promise<void>
+	create(reserva: Reserva): Promise<Reserva>
 	findByMesaId(mesaId: number): Promise<Reserva | null>
 }
