cc41a12642446d8300ddf21d60932930047011d2
Erick Barros Ferreira Gomes <erickgomes.developer@gmail.com>
2025-06-07 14:46:38 -0300

    test: removido testes que não são mais necessários

--- backend/src/http/controllers/AtualizarStatusReservaController.test.ts
+++ backend/src/http/controllers/AtualizarStatusReservaController.test.ts
@@ -1,17 +1,17 @@
-import { describe, it, expect, afterAll } from 'vitest'
-import { setup } from '../../app'
 import supertest from 'supertest'
-import { checkRouteExists } from '../../utils/checkRouteExists'
+import { afterAll, describe, expect, it } from 'vitest'
+import { setup } from '../../app'
 import { env } from '../../env'
+import { checkRouteExists } from '../../utils/checkRouteExists'
 import { getConnection } from '../../Datenbank/configdb'
 
-describe('ConfirmarReservaController', async () => {
+describe('AtualizarStatusReservaController', async () => {
 	const app = await setup()
 	afterAll(async () => {
 		const connection = getConnection()
 		await connection.close()
 	})
-	it.skip('deve ser possível confirmar uma reserva', async () => {
+	it('deve ser possível confirmar uma reserva', async () => {
 		const newReserva = await supertest(app).post('/api/reservas').send({
 			mesaId: 1,
 			nomeResponsavel: 'João Silva',
@@ -36,5 +36,23 @@ describe('ConfirmarReservaController', async () => {
 			env.GARCOM_ID_RANDOM,
 		)
 		expect(response.body.reserva).toHaveProperty('status', 'confirmada')
+	})
+	it('deve ser possível cancelar uma reserva', async () => {
+		const newReserva = await supertest(app).post('/api/reservas').send({
+			mesaId: 2,
+			nomeResponsavel: 'John Doe',
+			data: '2025-07-06',
+			hora: '13:00',
+			quantidadePessoas: 4,
+		})
+		const url = `/api/reservas/${newReserva.body.reserva.id}`
+		const response = await supertest(app).patch(url).send({
+			status: 'cancelada',
+		})
+		expect(checkRouteExists(response, 'PATCH', url)).toBe(true)
+		expect(response.status).toBe(200)
+		expect(response.body).toHaveProperty('reserva')
+		expect(response.body.reserva).toHaveProperty('id')
+		expect(response.body.reserva).toHaveProperty('status', 'cancelada')
 	})
 })
